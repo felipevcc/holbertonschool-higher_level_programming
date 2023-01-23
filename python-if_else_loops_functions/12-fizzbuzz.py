0af8d7fd9678119270751c729da234a95203c840
felipevcc <felipevillamizarc@gmail.com>
2023-01-23 14:14:24 -0500

    updates files 6 and 12

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def fizzbuzz():
-    for num in range (1, 101):
+    for num in range(1, 101):
         if (num % 3 == 0 and num % 5 == 0):
             print("FizzBuzz", end=" ")
         elif (num % 3 == 0):
--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
-for num1 in range (10):
-    for num2 in range (num1 + 1, 10):
+for num1 in range(10):
+    for num2 in range(num1 + 1, 10):
         if (num1 == 8):
             print("{}{}".format(num1, num2))
         else:
