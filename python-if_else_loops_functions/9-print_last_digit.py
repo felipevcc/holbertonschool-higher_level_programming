6c6d111c358f6b085f25a5315fb34fca2d47f775
felipevcc <felipevillamizarc@gmail.com>
2023-01-23 14:31:09 -0500

    file 9 update

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -3,6 +3,6 @@ def print_last_digit(number):
     if (number >= 0):
         last_digit = number % 10
     else:
-        last_digit = abs(number) % 10 
+        last_digit = abs(number) % 10
     print(last_digit, end="")
     return last_digit
