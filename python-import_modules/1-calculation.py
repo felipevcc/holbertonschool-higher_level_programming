31b7d14c0b7eed2e6b0f2346a03440b53c7af2ba
felipevcc <felipevillamizarc@gmail.com>
2023-01-24 09:28:48 -0500

    file 1

---
+++ python-import_modules/1-calculation.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import calculator_1
+if __name__ == '__main__':
+    a = 10
+    b = 5
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
