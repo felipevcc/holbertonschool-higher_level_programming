07fb4fc574f898158b4db6c773ad7207b507da98
felipevcc <felipevillamizarc@gmail.com>
2023-01-30 11:06:12 -0500

    update file 7, solution pycodestyle

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-def check_tuple(tuple):
-    if (len(tuple) == 0):
-        tuple = (0, 0)
-    elif (len(tuple) == 1):
-        tuple = (tuple[0], 0)
-    return tuple
 def add_tuple(tuple_a=(), tuple_b=()):
-    tuple_a = check_tuple(tuple_a)
-    tuple_b = check_tuple(tuple_b)
+    if (len(tuple_a) == 0):
+        tuple_a = (0, 0)
+    elif (len(tuple_a) == 1):
+        tuple_a = (tuple_a[0], 0)
+    if (len(tuple_b) == 0):
+        tuple_b = (0, 0)
+    elif (len(tuple_b) == 1):
+        tuple_b = (tuple_b[0], 0)
     return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
