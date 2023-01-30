9823542e2050f466078d0b023dfdeea3a6455448
felipevcc <felipevillamizarc@gmail.com>
2023-01-30 11:02:32 -0500

    update file 8

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if (not sentence):
-        return None
+    if (len(sentence) == 0):
+        return (0, None)
     return (len(sentence), sentence[0])
