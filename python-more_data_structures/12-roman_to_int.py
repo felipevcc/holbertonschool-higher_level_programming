2108f485a23e6f7bebaae21b448c29005cbf3023
felipevcc <felipevillamizarc@gmail.com>
2023-01-30 19:03:12 -0500

    file 12 - roman to integer

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    roman = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
-
+    roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    total = 0
+    if (not roman_string or type(roman_string) != str):
+        return total
+    for i in range(len(roman_string) - 1, -1, -1):
+        num_roman = roman[roman_string[i]]
+        if (4 * num_roman < total):
+            total -= num_roman
+        else:
+            total += num_roman
+    return total
