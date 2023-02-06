5c14f4eef0c6d08f0fe50c9156279a9638ffdc39
felipevcc <felipevillamizarc@gmail.com>
2023-02-06 10:44:07 -0500

    update files

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 class Square:
-    # Class that defines a square
+    """Class that defines a square"""
     pass
--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 class Square:
-    # Class that defines a square by its size
+    """Class that defines a square by its size"""
     def __init__(self, size):
         self.__size = size
