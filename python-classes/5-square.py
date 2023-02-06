e5b0b39514c8f80fd350250f2c3378dce54e0e74
felipevcc <felipevillamizarc@gmail.com>
2023-02-06 12:06:33 -0500

    files

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -17,13 +17,13 @@ class Square:
         return self.__size
 
     @size.setter
-    def size(self, size):
+    def size(self, value):
         """Type and value validation"""
-        if not isinstance(size, int):
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        elif size < 0:
+        elif value < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size
+        self.__size = value
 
     def area(self):
         """Returns the current square area"""
