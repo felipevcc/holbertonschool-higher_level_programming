297d47bd2d7e4d87951a7099188c1ec2ce5a0886
felipevcc <felipevillamizarc@gmail.com>
2023-02-09 11:22:06 -0500

    files 5

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Function that indents text"""
+
+
+def text_indentation(text):
+    """String validation"""
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    """Separators"""
+    sep = [".", "?", ":"]
+    idx_prev = 0
+
+    """Prints the text"""
+    for i in range(len(text)):
+        if i == len(text) - 1:
+            print(text[idx_prev:i + 1], end="")
+        elif text[i] in sep:
+            print(text[idx_prev:i + 1] + '\n')
+            idx_prev = i + 2
