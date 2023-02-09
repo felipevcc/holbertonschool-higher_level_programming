0eaf14da1c839ef28a0aab541d416ca132c66888
felipevcc <felipevillamizarc@gmail.com>
2023-02-08 19:53:50 -0500

    update files 2

--- python-test_driven_development/tests/2-main.py
+++ python-test_driven_development/tests/2-main.py
@@ -4,10 +4,9 @@ from sys import path
 path.append('../')
 matrix_divided = __import__('2-matrix_divided').matrix_divided
 
-"""matrix = [
+matrix = [
     [1, 2, 3],
     [4, 5, 6]
-]"""
-matrix = [[1, 2], [3, 4]]
-print(matrix_divided(matrix, float('inf')))
+]
+print(matrix_divided(matrix, 3))
 print(matrix)
